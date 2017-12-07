1af790d559930ab79bf5d75a4fb1a6fc87e7f7f6
James-Alex Matthews <vamtiger@vamtiger.com>
2017-12-07 23:47:24 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,14 +1,4 @@
 /// <reference types="node" />
-/// <reference types="bluebird" />
-import { PathLike } from 'fs';
-import * as Promise from 'bluebird';
-export declare type VamtigerReadDirectoryPath = PathLike | string;
-export declare type VamtigerReadDirectoryEntry = string;
-export declare type VamtigerReadDirectoryEntries = VamtigerReadDirectoryEntry[];
-export declare type VamtigerReadDirectoryOptions = VamtigerReadDirectoryOptionsObject | BufferEncoding;
-export interface VamtigerReadDirectoryOptionsObject {
-    encoding?: BufferEncoding;
-}
-export declare type VamtigerReadDirectory = (path: VamtigerReadDirectoryPath, options?: VamtigerReadDirectoryOptions) => Promise<VamtigerReadDirectoryEntries>;
-declare const _default: VamtigerReadDirectory;
+import { readdir } from 'fs';
+declare const _default: typeof readdir.__promisify__;
 export default _default;
--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const Promise = require("bluebird");
-exports.default = Promise.promisify(fs_1.readdir);
+const util_1 = require("util");
+exports.default = util_1.promisify(fs_1.readdir);
 //# sourceMappingURL=index.js.map
