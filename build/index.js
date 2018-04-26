97a7f9a6370e7419d7867d53cf8f2d01870b6f08
James-Alex Matthews <vamtiger@vamtiger.com>
2018-04-26 19:23:56 +0200

    vamtiger-node-typescript-commit

--- build/index.js
+++ build/index.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const util_1 = require("util");
-exports.default = util_1.promisify(fs_1.readdir);
+const BlueBird = require("bluebird");
+exports.default = util_1.promisify && util_1.promisify(fs_1.readdir) || BlueBird.promisify(fs_1.readdir);
 //# sourceMappingURL=index.js.map
