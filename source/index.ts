e1d04eda9f608aa8ca3b46260524b06e2f70f4f0
James-Alex Matthews <vamtiger@vamtiger.com>
2018-04-26 19:23:54 +0200

    vamtiger-node-typescript-commit

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,5 @@
 import { readdir, PathLike } from 'fs';
 import { promisify } from 'util';
+import * as BlueBird from 'bluebird';
 
-export default promisify(readdir);
+export default promisify && promisify(readdir) || BlueBird.promisify(readdir);
